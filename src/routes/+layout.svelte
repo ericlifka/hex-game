4ac9813b3005eabb2dd2bdf846d095c2af494ef1
Eric Lifka <eric.lifka@gmail.com>
2023-04-28 23:49:25 -0400

    set up global styles and variables

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+
+    import { object_without_properties } from "svelte/internal";
+
+</script>
+<svelte:head>
+    <style>
+        :root {
+            --color-bg: #18202e;
+            --color-text: #b8e6e3;
+        }
+
+        body {
+            padding: 0;
+            margin: 0;
+            font-size: 18px;
+            font-family: monospace;
+            background-color: var(--color-bg);
+            color: var(--color-text);
+        }
+    </style>
+</svelte:head>
+
+<slot />
